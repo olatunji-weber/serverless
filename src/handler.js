6cf34583fc819ce6f9707df8cb85582be67ee43a
Olatunji Olayinka <olatunji.weber@gmail.com>
2024-01-26 07:11:42 +0200

    Made corrections to serverless.yml and handler.js...

--- src/handler.js
+++ src/handler.js
@@ -3,5 +3,6 @@
 module.exports.generateRandomNumber = (event) => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The generated Random Integer is: ", randomNumber);
+  console,log("YINKUZE DID THIS....")
   return randomNumber;
 };
